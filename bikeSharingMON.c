ee28b177ff8b8460abf534e8b1ad1a233a7f0951
ToniVarettoni <tvarettoni@itba.edu.ar>
2023-12-05 16:22:12 -0300

    bikesharingmon update
    empezamos a sacar los datos

--- bikeSharingMON.c
+++ bikeSharingMON.c
@@ -13,8 +13,8 @@ int checkParams(char* bikes, char*stations, int startYear, int endYear){
     if(startYear != 0 && endYear != 0){
         if(endYear < startYear) return 0;
     }
-    if(strcmp(bikes, "bikes.csv") != 0) return 0;
-    if(strcmp(stations,"stations.csv") != 0) return 0;
+    if(strcmp(bikes, "bikesMON.csv") != 0) return 0;
+    if(strcmp(stations,"stationsMON.csv") != 0) return 0;
     return 1;
 }
 
@@ -27,21 +27,27 @@ int main(int argc, char * argv[]){
     bikes = argv[1];
     stations = argv[2];
 
-    if(argc > 2){
-        startYear = atoi(argv[3]);
-        if(argc == 4) endYear = atoi(arg[4]);
-    }
-
-    int a;
-    if((a = checkParams(bikes, stations, startYear, endYear)) == 0){
+    if(argc == 3 || argc == 4){
+            startYear = atoi(argv[3]);
+            if(argc == 4) endYear = atoi(arg[4]);
+        }
+    } else{
         printf("Error! Los parametros son incorrectos \n");
         return ERROR;
     }
 
+
     FILE * bikesCsv = fopen(bikes, "r");
     FILE * stationsCsv = fopen(stations, "r");
 
+    char * lineaActual;
+    unsigned long startStationId, endStationId;
+    struct tm startDate, endDate;
+    char isMember;
+    while(fscanf(bikesCsv, "%d-%d-%d %d:%d:%d;%ld;%d-%d-%d %d:%d:%d;%ld;%c", startDate.)){
 
+
+    }
 
 
 
