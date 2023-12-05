9850a4d049354f1dc5718b9b54b2d93f92723712
isa-conde <152538389+isa-conde@users.noreply.github.com>
2023-12-04 22:21:53 -0300

    Add files via upload

--- bicicletasADT.c
+++ bicicletasADT.c
@@ -2,52 +2,135 @@
 
 #define DAYS_OF_WEEK 7
 
-typedef struct {
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int second;
-} tDate;                            // nose si hay una mejor forma de hacer esto
 
 typedef struct ride{
     tDate startDate;
     tDate endDate;
-    size_t endStationId;
-    char isMember;                  // Guardo el bikeType aunque no lo use?????? (no lo guarde por ahora)
+    char isMember;                      // me interesa guardar el isMember en cada una?(solo lo uso al registrar uno nuevo)
+                                        // Guardo el bikeType aunque no lo use?????? (no lo guarde por ahora)
+    struct ride * next;
 } tRide;
+
+
+typedef struct route{
+    long endStationId;
+    tRide * rides;
+    long rideCount;
+    struct route * next;                                                  
+} tRoute;
 
 typedef struct station{
     char * name;
-    size_t id;
-    double latitude;
+    long id;
+    double latitude;                   // Guardo la lat y la long aunque no las use??? (las guarde por ahora)
     double longitude;
-    tRide * rides;
-    size_t memberRides;
-    size_t casualRides;
+    tRoute * routes;
+    long memberRides;
+    long casualRides;
     struct station * next;
 } tStation;
 
 typedef struct cityCDT{
     tStation * stations;
-    size_t ridesPerDay[DAYS_OF_WEEK];
-    size_t stationCount;
+    long ridesPerDay[DAYS_OF_WEEK];
+    long stationCount;
 } cityCDT;
 
 
 
 
 cityADT newCity(void){
-    return calloc(1, sizeof(cityCDT));
+    cityADT new = calloc(1, sizeof(cityCDT));       //FALTARIA CHEQUEAR LO DE NULL        
+
+    return new;
 }
 
+static
+tStation * addStationRec(tStation * station, char * name, long id, double latitude, double longitude, int * flag){
+        	                                                //FALTARIA CHEQUEAR PARAMETROS (invalido => return -1)
+    if(station == NULL || station->id > id){
+        tStation * new = malloc(sizeof(tStation));         //FALTARIA CHEQUEAR NULL
+        new->name = name;
+        new->id = id;
+        new->latitude = latitude;
+        new->longitude = longitude;
+        new->routes = NULL;
+        new->memberRides = new->casualRides = 0;
+        new->next = station;
+        *flag = 1;
+        return new;
+    }
+    if(station->id < id)
+        station->next = addStationRec(station->next, name, id, latitude, longitude, flag);
+    return station;
+}
 
-
-void addStation(cityADT city, char * name, size_t id, double latitude, double longitude){
+int addStation(cityADT city, char * name, long id, double latitude, double longitude){
     int flag = 0;
-    addStationRec(city->stations, name, id, latitude, longitude, &flag);
+    city->stations = addStationRec(city->stations, name, id, latitude, longitude, &flag);
     city->stationCount += flag;
+    return flag;
 }
+
+static
+tRoute * addRideRec(tRoute * route, tDate startDate, tDate endDate, long endStationId, char isMember, int * flag){
+    if(route->endStationId = endStationId){
+        
+    }   
+}
+
+int addRide(cityADT city, long startStationId, tDate startDate, tDate endDate, long endStationId, char isMember){
+                                                    //FALTARIA CHEQUEAR PARAMETROS (invalido => return -1)
+    tStation * aux = city->stations;
+    tStation * station;
+    int foundStart = 0;
+    int foundEnd = 0;
+
+    // queda feo con tantos && pero si lo separo recorro dos veces al pedo. como hacerlo mas lindo?
+
+    while(aux != NULL && aux->id < startStationId && aux->id < endStationId && (!foundStart || !foundEnd)){
+        if(aux->id == startStationId){
+            station = aux;
+            foundStart = 1;
+        }
+        if(aux->id == endStationId)
+            foundEnd = 1;
+
+        aux = aux->next;
+    }
+    if((!foundStart || !foundEnd))
+        return -1;                      // el start id o el end id no existen => error!!
+
+    int flag = 0;
+    station->routes = addRideRec(station->routes, startDate, endDate, endStationId, isMember, &flag);
+
+    
+    tRoute auxRoute = station->routes;
+    
+    if(auxRoute->endStationId = endStationId){
+
+    }
+
+        
+
+}
+
+typedef struct ride{
+    tDate startDate;
+    tDate endDate;
+    char isMember;                      // me interesa guardar el isMember en cada una?(solo lo uso al registrar uno nuevo)
+                                        // Guardo el bikeType aunque no lo use?????? (no lo guarde por ahora)
+    struct ride * next;
+} tRide;
+
+
+typedef struct route{
+    long endStationId;
+    tRide * rides;
+    long rideCount;
+    struct route * next;                                                  
+} tRoute;
+
+
 
 
