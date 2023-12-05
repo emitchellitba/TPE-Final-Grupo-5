8bd8050aca36f8e9e40e4a2f3388c122069a2b8a
emitchellitba <141164716+emitchellitba@users.noreply.github.com>
2023-12-05 19:31:24 -0300

    Update a bikeSharingNYC

--- bikeSharingNYC.c
+++ bikeSharingNYC.c
@@ -1,7 +1,7 @@
 #include "bicicletasADT.h"
 #include <stdio.h>
 
-#define ERROR -1
+#define PARAM_ERROR -1
 
 int checkParams(char* bikes, char*stations, int startYear, int endYear){
 
@@ -20,7 +20,6 @@ int main(int argc, char * argv[]){
     char* bikes, stations;
 
     if(argc < 3 || argc > 5) {
-        
         printf("Cantidad invalida de parametros.\n");
         return PARAM_ERROR;
     } else {
@@ -47,19 +46,25 @@ int main(int argc, char * argv[]){
     unsigned long startStationId, endStationId;
     struct tm startDate, endDate;
     char * memberState;
+
+    while(fgets(stationsCsv, "r")) {
+        
+    }
     
-    while(fscanf(bikesCsv, "%d-%d-%d %d:%d:%d;%ld;%d-%d-%d %d:%d:%d;%ld;%*s;%s", &startDate.tm_year, &startDate.tm_mon, &startDate.tm_mday, 
-    &startDate.tm_hour, &startDate.tm_min, &startDate.tm_sec, &startStationId, &endDate.tm_year, &endDate.tm_mon, &endDate.tm_mday, &endDate.tm_hour,
-    &endDate.tm_min, &endDate.tm_sec, &endStationId, memberState) != EOF){                     //TAMBIEN PUEDE SER DISTINTO DE CERO
-        addRide(nyc, startStationId, startDate, endDate, endStationId, (char) strcmp("member", memberState) = 0);
-    }
+    // while(fscanf(bikesCsv, "%d-%d-%d %d:%d:%d;%ld;%d-%d-%d %d:%d:%d;%ld;%*s;%s", &startDate.tm_year, &startDate.tm_mon, &startDate.tm_mday, 
+    // &startDate.tm_hour, &startDate.tm_min, &startDate.tm_sec, &startStationId, &endDate.tm_year, &endDate.tm_mon, &endDate.tm_mday, &endDate.tm_hour,
+    // &endDate.tm_min, &endDate.tm_sec, &endStationId, memberState) != EOF){                     //TAMBIEN PUEDE SER DISTINTO DE CERO
+    //     addRide(nyc, startStationId, startDate, endDate, endStationId, strcmp("member", memberState) == 0);
+    // }
 
-    char * name;
-    unsigned long stationId;
+    // char * name;
+    // unsigned long stationId;
 
-    while(fscanf(bikesCsv, "%s;%*f;%*f;%ld", name, &stationId) != EOF){                     //TAMBIEN PUEDE SER DISTINTO DE CERO
-        addStation(nyc, name, stationId);
-    }
+    // while(fscanf(bikesCsv, "%s;%*f;%*f;%ld", name, &stationId) != EOF){                     //TAMBIEN PUEDE SER DISTINTO DE CERO
+    //     addStation(nyc, name, stationId);
+    // }
+
+    
 
     fclose(bikesCsv);
     fclose(stationsCsv);
