2c2a9514a60e51da2e4c54be9d7e49e8bb264473
isa-conde <152538389+isa-conde@users.noreply.github.com>
2023-12-05 14:10:16 -0300

    Add files via upload
    Cambio del CDT

--- bicicletasADT.c
+++ bicicletasADT.c
@@ -1,33 +1,38 @@
 #include "bicicletasADT.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <time.h>
 
 #define DAYS_OF_WEEK 7
+#define BLOCK 50
 
-typedef struct destiny {
-    long id;
-    tRide * rides;
-} tDestiny;
 
 typedef struct ride{
-    tDate startDate;
-    tDate endDate;
+    struct tm start_date;
+    struct tm end_date;
     struct ride * next;
 } tRide;
 
+typedef struct destiny {
+    unsigned long index;
+    tRide * rides;
+} tDestiny;
+
 typedef struct station{
     char * name;
-    long id;
+    unsigned long id;
     tDestiny * destinies;
-    long oldestDestiny;
-    tDate oldestDate;
-    long memberRides;
-    long casualRides;
-    struct station * next;
+    unsigned long destiniesCount;
+    unsigned long oldestDestinyIdx;
+    struct tm oldest_date;
+    unsigned long memberRides;
+    unsigned long casualRides;
 } tStation;
 
 typedef struct cityCDT{
     tStation * stations;
-    long ridesPerDay[DAYS_OF_WEEK];
-    long stationCount;
+    unsigned long ridesPerDay[DAYS_OF_WEEK];
+    unsigned long stationCount;
 } cityCDT;
 
 
@@ -39,63 +44,107 @@ cityADT newCity(void){
     return new;
 }
 
+
+int addStation(cityADT city, char * name, unsigned long id){
+    int esta = 0;
+    unsigned long i;
+    for(i = 0; i < city->stationCount && !esta; i++){
+        if(city->stations[i].id == id)
+            esta = 1;
+    }
+    if(!esta){
+        if(i % BLOCK == 0){
+            city->stations = realloc(city->stations, (i + BLOCK) * sizeof(tStation));
+            //FALTARIA CHEQEUAR NULL
+        }
+        city->stations[i].name = name;
+        city->stations[i].id = id;
+        city->stations[i].destinies = NULL;
+        city->stations[i].destiniesCount = 0;
+        city->stations[i].memberRides = city->stations[i].casualRides = 0;
+        city->stationCount++;
+    }
+    return !esta;
+}
+
+
 static
-tStation * addStationRec(tStation * station, char * name, long id, int * flag){
-    if(station == NULL || station->id > id){
-        tStation * new = malloc(sizeof(tStation));         //FALTARIA CHEQUEAR NULL
-        new->name = name;
-        new->id = id;
-        new->routes = NULL;
-        new->memberRides = new->casualRides = 0;
-        new->next = station;
-        *flag = 1;
+int dateCompare(struct tm d1, struct tm d2){
+    int diff;
+    if((diff = d1.tm_year - d2.tm_year) == 0)
+        if((diff = d1.tm_mon - d2.tm_mon) == 0)
+            if((diff = d1.tm_mday - d2.tm_mday) == 0)
+                if((diff = d1.tm_hour - d2.tm_hour) == 0)
+                    if((diff = d1.tm_min - d2.tm_min) == 0)
+                        diff = d1.tm_sec - d2.tm_sec;
+    return diff;
+}
+
+static
+tRide * addRideRec(tRide * ride, struct tm start_date, struct tm end_date){
+    int cmp;
+    if(ride == NULL || (cmp = dateCompare(start_date, ride->start_date)) < 0){
+        tRide * new = malloc(sizeof(tRide));                    //FALTARIA CHEQUEAR NULL
+        new->start_date = start_date;
+        new->end_date = end_date;
+        new->next = ride;
         return new;
     }
-    if(station->id < id)
-        station->next = addStationRec(station->next, name, id, flag);
-    return station;
-}
-
-int addStation(cityADT city, char * name, long id){
-        	                                                //FALTARIA CHEQUEAR PARAMETROS (invalido => return -1)
-    int flag = 0;
-    city->stations = addStationRec(city->stations, name, id, &flag);
-    city->stationCount += flag;
-    return flag;
+    if(cmp > 0)
+        addRideRec(ride->next, start_date, end_date);
+    return ride;
 }
 
 
-
-int addRide(cityADT city, long startStationId, tDate startDate, tDate endDate, long endStationId, char isMember){
-                                                    //FALTARIA CHEQUEAR PARAMETROS (invalido => return -1)
-    tStation * aux = city->stations;
-    tStation * station;
+// Queda muy larga. Podriamos modularizar? Quizas no tiene sentido igual pq no lo volveriamos a usar
+int addRide(cityADT city, unsigned long startStationId, struct tm start_date, struct tm end_date, unsigned long endStationId, char isMember){
+                                                    //FALTARIA CHEQUEAR PARAMETROS (invalido => return -1) (fechas, is Member maybe)
+                                                    //FALTARIA CONSIDERAR CASO QUE SALGA Y VUELVA AL MISMO LUGAR (o directo en las funciones?)
+    tStation station;
+    unsigned long i, endIndex;
     int foundStart = 0;
     int foundEnd = 0;
 
-    // queda feo con tantos && pero si lo separo recorro dos veces al pedo. como hacerlo mas lindo?
-
-    while(aux != NULL && aux->id < startStationId && aux->id < endStationId && (!foundStart || !foundEnd)){
-        if(aux->id == startStationId){
-            station = aux;
+    for(i = 0; i < city->stationCount && (!foundStart || !foundEnd); i++){
+        if(city->stations[i].id == startStationId){
+            station = city->stations[i];
             foundStart = 1;
         }
-        if(aux->id == endStationId)
+        if(city->stations[i].id == endStationId){
+            endIndex = i;
             foundEnd = 1;
-
-        aux = aux->next;
+        }
     }
-    if((!foundStart || !foundEnd))
-        return -1;                      // el start id o el end id no existen => error!!
-
-    int flag = 0;
-    station->rides = addRideRec(station->rides, startDate, endDate, endStationId, isMember, &flag);
-
-
+    if((foundStart && foundEnd)){
+        int foundDestiny = 0;
+        for(i = 0; i < station.destiniesCount && !foundDestiny; i++){
+            if(station.destinies[i].index == endIndex){
+                station.destinies[i].rides = addRideRec(station.destinies[i].rides, start_date, end_date);
+                foundDestiny = 1;
+            }
+        }
+        if(!foundDestiny){
+            if(i % BLOCK == 0){
+                station.destinies = realloc(station.destinies, (i + BLOCK) * sizeof(tDestiny));
+                //FALTARIA CHEQUeAR NULL
+            }
+            station.destinies[i].index = endIndex;
+            station.destinies[i].rides = addRideRec(NULL, start_date, end_date);
+            station.destiniesCount++;
+        }
         
+        if(station.memberRides + station.casualRides == 0 || dateCompare(start_date, station.oldest_date) < 0){
+            station.oldestDestinyIdx = endIndex;
+            station.oldest_date = start_date;
+        }
 
+        if(isMember)
+            station.memberRides++;
+        else
+            station.casualRides++;
+        
+        mktime(&start_date);
+        city->ridesPerDay[start_date.tm_wday]++;
+    }
+    return 0;
 }
-
-
-
-
