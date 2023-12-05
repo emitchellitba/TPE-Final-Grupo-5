5a9ba4fa3841bd4696e6344a1f03585f4802421c
isa-conde <152538389+isa-conde@users.noreply.github.com>
2023-12-05 14:08:30 -0300

    Add files via upload

--- bicicletasADT.h
+++ bicicletasADT.h
@@ -1,28 +1,20 @@
 #ifndef BICICLETASADT_H
 #define BICICLETASADT_H
 
+#include <time.h>
+
 typedef struct cityCDT * cityADT;
 
-typedef struct {
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int second;                      // vale ponerlo en el .h ????????
-} tDate;                            // nose si hay una mejor forma de hacer esto
 
-
-
-cityADT newCity();
+cityADT newCity(void);
 
 // Agrega una estacion con los datos dados, vacia en viajes
 // devuelve 1 si agrega, 0 si no, -1 en caso de error en los parametros                               
-int addStation(cityADT city, char * name, size_t id);
+int addStation(cityADT city, char * name, unsigned long id);
 
 // Agrega un viaje con los datos dados, dentro de la lista de stations en la estacion de inicio
-// devuelve 1 si agrega, 0 si no, -1 en caso de error en los parametros  
-int addRide(cityADT city, size_t startStationId, tDate startDate, tDate endDate, size_t endStationId, char isMember);
+// -1 en caso de error en los parametros, 0 en cualquier otro caso
+int addRide(cityADT city, unsigned long startStationId, struct tm startDate, struct tm endDate, unsigned long endStationId, char isMember);
 
 
 
