e04e50731ca1298a0c866a178101c61a2e8ded69
isa-conde <isconde@itba.edu.ar>
2023-12-06 17:36:31 -0300

    Update bicicletasADT.h

--- bicicletasADT.h
+++ bicicletasADT.h
@@ -10,7 +10,7 @@ cityADT newCity(void);
 
 // Agrega una estacion con los datos dados, vacia en viajes
 // devuelve 1 si agrega, 0 si no, -1 en caso de error en los parametros                               
-int addStation(cityADT city, char * name, unsigned long id);
+int addStation(cityADT city, char * name, size_t id);
 
 // Agrega un viaje con los datos dados, dentro de la lista de stations en la estacion de inicio
 void addRide(cityADT city, unsigned long startStationId, struct tm startDate, struct tm endDate, unsigned long endStationId, int isMember);
