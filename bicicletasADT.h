017079e16dd8bb0f5b6d3c992cf5c43baf4e707a
isa-conde <isconde@itba.edu.ar>
2023-12-08 18:15:02 -0300

    comentarios

--- bicicletasADT.h
+++ bicicletasADT.h
@@ -10,12 +10,14 @@ typedef struct cityCDT * cityADT;
 
 cityADT newCity(void);
 
-// Agrega una estacion con los datos dados, vacia en viajes
-// devuelve 1 si agrega, 0 si no, -1 en caso de error en los parametros                               
+/* Agrega una estacion con los datos dados, vacia en viajes. Devuelve 1 si agrega, 0 si no */                         
 int addStation(cityADT city, char * name, size_t id);
 
-// Agrega un viaje con los datos dados, dentro de la lista de stations en la estacion de inicio
+/* Agrega un viaje con los datos dados. Si las estaciones de incio o fin no existen, se ignora el viaje */
 void addRide(cityADT city, size_t startStationId, struct tm startDate, struct tm endDate, size_t endStationId, int isMember);
+
+/* Se libera la memoria reservada */
+void freeCity(cityADT city);
 
 void getIdexByRank(cityADT city, int idexVec[]);
 
@@ -30,8 +32,6 @@ void getIndexByRank(cityADT city, int indexVec[]);
 void getIndexByAlph(cityADT city, int indexVec[]);
 
 void getOldest(cityADT city, int index, char ** nameStart, char ** nameEnd, struct tm * oldestTime);
-
-void freeCity(cityADT city);
 
 size_t getStartedRides(cityADT city, int index);
 
