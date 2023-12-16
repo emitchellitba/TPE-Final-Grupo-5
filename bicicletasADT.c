15e7ffd9093e1c90715b4d4587c00b0d7dffefb6
BrunTac <btaccone@itba.edu.ar>
2023-12-16 17:59:22 -0300

    Update bicicletasADT.c

--- bicicletasADT.c
+++ bicicletasADT.c
@@ -402,9 +402,7 @@ tMostPopular getMostPopular(tStation * station, int startYear, int endYear){
         char * maxName = station->destinies->name;
 
         /*Se recorren todos los destinos y se compara con el máximo registrado*/
-        while(aux != NULL){
-            station->destinies = getMostPopularRec(station->destinies, startYear, endYear, &maxName, maxRides);
-        }
+        getMostPopularRec(station->destinies, startYear, endYear, &maxName, maxRides);
       
         mostPopular.cantRides = maxRides;
         mostPopular.endName = maxName;
